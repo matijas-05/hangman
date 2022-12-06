a4cbd226635bea00d511249b13e3250cde4ad422
Mateusz Wojtyna <w_mateusz@hotmail.com>
2022-12-06 21:33:08 +0100

    feat: get random word from file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use rand::{prelude::thread_rng, Rng};
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", get_random_noun().unwrap());
+}
+
+fn get_random_noun() -> Result<&'static str, std::io::Error> {
+    let raw = include_str!("nouns.txt");
+    let words = raw.split('\n').collect::<Vec<&str>>();
+    let random_index = thread_rng().gen_range(0..words.len());
+
+    Ok(words[random_index])
 }
