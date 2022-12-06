2f233ca3f8b3f6062ac14980efb29aa586c2c5d7
Mateusz Wojtyna <w_mateusz@hotmail.com>
2022-12-06 21:53:34 +0100

    refactor: replace get_random_noun() return type with &str

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use rand::{prelude::thread_rng, Rng};
 
 fn main() {
-    println!("{}", get_random_noun().unwrap());
+    println!("{}", get_random_noun());
 }
 
-fn get_random_noun() -> Result<&'static str, std::io::Error> {
+fn get_random_noun() -> &'static str {
     let raw = include_str!("nouns.txt");
     let words = raw.split('\n').collect::<Vec<&str>>();
     let random_index = thread_rng().gen_range(0..words.len());
 
-    Ok(words[random_index])
+    words[random_index]
 }
